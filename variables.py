7dcbc1b4629fe7a28583e726177d166d1f4ea7e8
Leito <leogturzi@gmail.com>
2021-03-22 22:47:35 -0300

    update

---
+++ variables.py
@@ -0,0 +1,14 @@
+# This module just contains variables
+
+results = "ui-search-search-result__quantity-results"
+pageLink = "andes-pagination__link ui-search-link"
+linkRef = "ui-search-result__content ui-search-link"
+carModels = [
+    "https://autos.mercadolibre.com.ar/renault/clio/",
+    "https://autos.mercadolibre.com.ar/chevrolet/celta/",
+    "https://autos.mercadolibre.com.ar/peugeot/206/",
+    "https://autos.mercadolibre.com.ar/honda/fit/",
+    "https://autos.mercadolibre.com.ar/peugeot/207/",
+    "https://autos.mercadolibre.com.ar/volkswagen/gol/",
+    "https://autos.mercadolibre.com.ar/volkswagen/fox/"
+    ]
